8edb99a62381b538e3ae57e06ac77e3c9799cb1c
Alex Hall <ach740@gmail.com>
2020-06-02 11:50:31 +0100

    Exercises in parts 1.1 to 1.3

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,15 @@
 # bounce.py
 #
 # Exercise 1.5
+
+start_height = 100.
+N_bounces = 10
+frac = 0.6
+
+height = start_height
+
+for i in range(N_bounces):
+    height = height * frac
+    bounce_idx = i + 1
+
+    print(bounce_idx, round(height, 4))
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,34 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+
+extra_payment_start_month = 60
+extra_payment_end_month = 108
+extra_payment = 1000.
+
+while principal > 0:
+    month = month + 1
+    if(extra_payment_start_month <= month and month <= extra_payment_end_month):
+	    monthly_payment = payment + extra_payment
+    else:
+        monthly_payment = payment
+    proposed_balance = principal * (1+rate/12) - monthly_payment
+    if(proposed_balance < 0.):
+        # This is our last monthly payment
+        monthly_payment = proposed_balance + monthly_payment
+        proposed_balance = 0.
+   
+    principal = proposed_balance
+
+    total_paid = total_paid + monthly_payment
+    print(month, total_paid, principal)
+
+
+print('Total paid', total_paid)
+print('Months taken', month)
